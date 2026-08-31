0079e98b9e6763986bdb2005d255a7cde6bf3fa4
Hector <hector.adjakpa@gmail.com>
2026-08-31 23:57:59 +0200

    k3s prod : correction de test

--- services/marketplace/seller-service/src/HBA.Merchants.Infrastructure/Migrations/20260831215640_DeriveModeleMembres.Designer.cs
+++ services/marketplace/seller-service/src/HBA.Merchants.Infrastructure/Migrations/20260831215640_DeriveModeleMembres.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HBA.Merchants.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HBA.Merchants.Infrastructure.Migrations
 {
     [DbContext(typeof(SellersDbContext))]
-    partial class SellersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260831215640_DeriveModeleMembres")]
+    partial class DeriveModeleMembres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
